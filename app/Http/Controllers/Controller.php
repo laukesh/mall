b0e3a7be1711f4b772ab0b17c1cbab967f461528
laukesh <52986997+laukesh@users.noreply.github.com>
2026-08-05 22:41:50 +0530

    fix(controller): add AuthorizesRequests, DispatchesJobs, ValidatesRequests traits so authorizeResource and helpers are available

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Controller extends BaseController
 {
-    // Base controller for application controllers
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
